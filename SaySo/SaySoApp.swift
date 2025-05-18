e5d6144bc2d0ed42bd01e81cac0ced6404fad381
Jenny Choi <jchoi766@usc.edu>
2025-05-17 19:59:50 -0700

    Initial Commit

---
+++ SaySo/SaySoApp.swift
@@ -0,0 +1,17 @@
+//
+//  SaySoApp.swift
+//  SaySo
+//
+//  Created by Jenny Choi on 5/17/25.
+//
+
+import SwiftUI
+
+@main
+struct SaySoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SaySoTests/SaySoTests.swift
@@ -0,0 +1,17 @@
+//
+//  SaySoTests.swift
+//  SaySoTests
+//
+//  Created by Jenny Choi on 5/17/25.
+//
+
+import Testing
+@testable import SaySo
+
+struct SaySoTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
